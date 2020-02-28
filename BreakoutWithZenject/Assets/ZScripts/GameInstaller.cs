ecf7770258d8135e53e5ff208587cb49a10a5f1e
ashrafuz <rawnok02@gmail.com>
2020-02-28 23:27:31 +0600

    zenject start , Lets do this!

---
+++ BreakoutWithZenject/Assets/ZScripts/GameInstaller.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using Zenject;
+
+public class GameInstaller : MonoInstaller {
+    public override void InstallBindings () {
+
+        //InstallPaddle
+        //InstallBall = with ball manager = factor?
+        //InstallBricks = with brick manager = factory?
+        //InstallScoreUI
+    }
+}
